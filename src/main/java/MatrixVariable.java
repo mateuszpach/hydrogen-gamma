3cd2ecda02aa08708d87f866835b460b4481b371
Lukasz <lukasgniecki@gmail.com>
2021-05-04 12:39:36 +0200

    MatrixVariable adjustment.

--- src/main/java/MatrixVariable.java
+++ src/main/java/MatrixVariable.java
@@ -1,9 +1,24 @@
 import java.util.ArrayList;
 
 public class MatrixVariable implements Variable {
-    final ArrayList<ArrayList<Double>> value;
 
-    MatrixVariable(ArrayList<ArrayList<Double>> value) {
+    private final Double[][] value;
+
+    MatrixVariable(Double[][] value) throws IllegalArgumentException {
+        if (value == null || value.length == 0 || value[0].length == 0)
+            throw new IllegalArgumentException();
         this.value = value;
+    }
+
+    public Double get(int i, int j) {
+        return value[i][j];
+    }
+
+    public Integer rowsNum() {
+        return value.length;
+    }
+
+    public Integer colsNum() {
+        return value[0].length;
     }
 }
