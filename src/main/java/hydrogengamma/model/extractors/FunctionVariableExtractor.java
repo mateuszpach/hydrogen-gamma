19e8d3b223b0d78bd0583270619180ee6db361c5
Lukasz <lukasgniecki@gmail.com>
2021-08-30 18:16:37 +0200

    Extractor classes for parser loader to use.

---
+++ src/main/java/hydrogengamma/model/extractors/FunctionVariableExtractor.java
@@ -0,0 +1,16 @@
+package hydrogengamma.model.extractors;
+
+import hydrogengamma.model.variables.FunctionVariable;
+
+public class FunctionVariableExtractor implements VariableExtractor<FunctionVariable> {
+    @Override
+    public FunctionVariable extract(String formula) {
+        return new FunctionVariable(formula);
+    }
+
+    @Override
+    public boolean verify(String formula) {
+        formula = formula.replaceAll("\\s+", "");// not a text so remove all whitespace left
+        return formula.length() >= 2 && formula.charAt(0) == '(' && formula.charAt(formula.length() - 1) == ')';
+    }
+}
