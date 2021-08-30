f1691528335524888499ad2dbb4ee38c0d1ee19f
Lukasz <lukasgniecki@gmail.com>
2021-08-30 17:39:46 +0200

    TileDecorator and TilesContainerDecorator added.

---
+++ src/main/java/hydrogengamma/model/TilesContainerDecorator.java
@@ -0,0 +1,32 @@
+package hydrogengamma.model;
+
+import hydrogengamma.vartiles.Tile;
+import hydrogengamma.vartiles.TileDecorator;
+
+import java.util.ArrayList;
+
+public class TilesContainerDecorator implements TilesContainer {
+
+    private final TilesContainer container; // decoratee
+    private final String labelPrefix;
+
+    public TilesContainerDecorator(TilesContainer container, String prefix) {
+        this.container = container;
+        labelPrefix = prefix;
+    }
+
+    public TilesContainerDecorator(TilesContainer container, String prefix, Tile tile) {
+        this(container, prefix);
+        addTile(tile);
+    }
+
+    @Override
+    public void addTile(Tile tile) {
+        container.addTile(new TileDecorator(tile, labelPrefix));
+    }
+
+    @Override
+    public ArrayList<Tile> getTiles() {
+        return container.getTiles();
+    }
+}
