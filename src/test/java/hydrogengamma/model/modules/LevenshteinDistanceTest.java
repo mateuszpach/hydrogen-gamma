ab66c5a25729490ab85e80b7e3241aff8f438fc1
mateuszpach <mateusz.pach.05@gmail.com>
2021-09-01 01:16:35 +0200

    Split text modules tests.

---
+++ src/test/java/hydrogengamma/model/modules/LevenshteinDistanceTest.java
@@ -0,0 +1,68 @@
+package hydrogengamma.model.modules;
+
+import hydrogengamma.model.TilesContainer;
+import hydrogengamma.model.variables.NumericVariable;
+import hydrogengamma.model.variables.TextVariable;
+import hydrogengamma.vartiles.NumericTile;
+import hydrogengamma.vartiles.Tile;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevenshteinDistanceTest {
+
+    @Test
+    void executeResult() {
+        // Given
+        TextVariable word1 = new TextVariable("Sunday");
+        TextVariable word2 = new TextVariable("Saturday");
+
+        TilesContainer container = Mockito.mock(TilesContainer.class);
+
+        // When
+        NumericVariable result = new LevenshteinDistance().execute(container, word1, word2);
+
+        // Then
+        NumericVariable expectedResult = new NumericVariable(3);
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void executeTiles() {
+        // Given
+        TextVariable word1 = new TextVariable("Sunday");
+        TextVariable word2 = new TextVariable("Saturday");
+
+        ArgumentCaptor<Tile> captor = ArgumentCaptor.forClass(Tile.class);
+        TilesContainer container = Mockito.mock(TilesContainer.class);
+
+        // When
+        new LevenshteinDistance().execute(container, word1, word2);
+        Mockito.verify(container).addTile(captor.capture());
+        List<Tile> tiles = captor.getAllValues();
+
+        // Then
+        assertEquals(1, tiles.size());
+
+        NumericVariable expectedResult = new NumericVariable(3);
+        Tile expectedTile = new NumericTile(expectedResult, "Levenshtein distance of");
+        assertEquals(expectedTile.getContent(), tiles.get(0).getContent());
+
+        assertEquals(expectedTile.getLabel(), tiles.get(0).getLabel());
+    }
+
+    @Test
+    void verify() {
+        TextVariable t = Mockito.mock(TextVariable.class);
+        NumericVariable x = Mockito.mock(NumericVariable.class);
+
+        assertTrue(new LevenshteinDistance().verify(t, t));
+
+        assertFalse(new LevenshteinDistance().verify(t));
+        assertFalse(new LevenshteinDistance().verify(x, t));
+    }
+}
---
+++ src/test/java/hydrogengamma/model/modules/LongestCommonSubstringsTest.java
@@ -0,0 +1,59 @@
+package hydrogengamma.model.modules;
+
+import hydrogengamma.model.TilesContainer;
+import hydrogengamma.model.variables.NumericVariable;
+import hydrogengamma.model.variables.TextVariable;
+import hydrogengamma.vartiles.TableTile;
+import hydrogengamma.vartiles.Tile;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestCommonSubstringsTest {
+
+    @Test
+    void executeTiles() {
+        // Given
+        TextVariable word1 = new TextVariable("abaabaab");
+        TextVariable word2 = new TextVariable("xdabaabxdxdabaab");
+
+        ArgumentCaptor<Tile> captor = ArgumentCaptor.forClass(Tile.class);
+        TilesContainer container = Mockito.mock(TilesContainer.class);
+
+        // When
+        new LongestCommonSubstrings().execute(container, word1, word2);
+        Mockito.verify(container).addTile(captor.capture());
+        List<Tile> tiles = captor.getAllValues();
+
+        // Then
+        assertEquals(1, tiles.size());
+
+        ArrayList<String> expectedResult = new ArrayList<>(Arrays.asList(
+                "abaab",
+                "abaab",
+                "abaab",
+                "abaab"
+        ));
+        Tile expectedTile = new TableTile(expectedResult, "Longest common substrings of");
+        assertEquals(expectedTile.getContent(), tiles.get(0).getContent());
+
+        assertEquals(expectedTile.getLabel(), tiles.get(0).getLabel());
+    }
+
+    @Test
+    void verify() {
+        TextVariable t = Mockito.mock(TextVariable.class);
+        NumericVariable x = Mockito.mock(NumericVariable.class);
+
+        assertTrue(new LongestCommonSubstrings().verify(t, t));
+
+        assertFalse(new LongestCommonSubstrings().verify(t, x));
+        assertFalse(new LongestCommonSubstrings().verify(t));
+    }
+}
